7647b79689e78aba72ac4aa7e179b110a5f02473
gortin <bielsanfru@gmail.com>
2025-04-29 21:24:37 -0300

    FIX: ajustes nas rotas

--- aluno/aluno_route.py
+++ aluno/aluno_route.py
@@ -8,36 +8,37 @@ def get_alunos():
     return jsonify(listar_alunos())
     
 
+
 @alunos_blueprint.route('/alunos/<int:id>', methods = ['GET'])
 def get_aluno(id):
     try:
         aluno = aluno_por_id(id)
         return jsonify(aluno)
     except AlunoNaoEncontrado:
-        return jsonify({'message':'Aluno não encontrado'}, 404)
+        return jsonify({'erro':'Aluno não encontrado'}), 404
         
 
 @alunos_blueprint.route('/alunos', methods = ['POST'])
-def criar_aluno():
+def create_aluno():
     dados = request.json
     adicionar_aluno(dados)
     return jsonify(dados), 201 
 
 
 @alunos_blueprint.route('/alunos/<int:id>', methods=['PUT'])
-def atualiza_aluno(id):
+def update_aluno(id):
     dados = request.json
     try:
         atualizar_aluno(id, dados)
         return jsonify(aluno_por_id(id))
     except AlunoNaoEncontrado:
-        return jsonify({'message':'Aluno não encontrado'}, 404)
+        return jsonify({'erro':'Aluno não encontrado'}), 404
         
 
 @alunos_blueprint.route('/alunos/<int:id>', methods=['DELETE'])
-def deletar_aluno(id):
+def delete_aluno(id):
     try:
         excluir_aluno(id)
         return '', 204 
     except AlunoNaoEncontrado:
-        return jsonify({'message':'Aluno não encontrado'}, 404)
+        return jsonify({'erro':'Aluno não encontrado'}), 404
--- professor/professor_route.py
+++ professor/professor_route.py
@@ -9,33 +9,35 @@ def getProfessores():
     return jsonify(listar_professores())
 
 @professores_blueprint.route('/professores/<int:id>', methods = ['GET'])
-def getProfessor(id):
+def get_professor(id):
     try:
         professor = professor_por_id(id)
         return jsonify(professor)
     except ProfessorNaoEncontrado:
-            return jsonify({'Erro': f'Ocorreu um erro: Professor não encontrado'}, 404)
-            
+        return jsonify({'erro': 'Professor não encontrado'}), 404
+        
         
 @professores_blueprint.route('/professores', methods = ['POST'])
-def createProfessor():
-        dados = request.json
-        adicionar_professor(dados)
-        return jsonify(dados)
+def create_professor():
+    dados = request.json
+    adicionar_professor(dados)
+    return jsonify(dados), 201
+
         
 @professores_blueprint.route('/professores/<int:id>', methods = ['PUT'])
-def updateProfessor(id):
-        dados = request.json
-        try: 
-            atualizar_professor(id, dados)
-            return jsonify(professor_por_id(id))
-        except ProfessorNaoEncontrado:
-            return jsonify({'Erro': f'Ocorreu um erro: Professor não foi atualizado'}, 404)
+def update_professor(id):
+    dados = request.json
+    try: 
+        atualizar_professor(id, dados)
+        return jsonify(professor_por_id(id))
+    except ProfessorNaoEncontrado:
+        return jsonify({'erro': 'Professor não encontrado'}), 404
                 
+
 @professores_blueprint.route('/professores/<int:id>', methods=['DELETE'])
-def deleteProfessor(id):
+def delete_professor(id):
     try:
         excluir_professor(id)
         return "", 204
     except ProfessorNaoEncontrado:
-        return jsonify({'Erro': f'Ocorreu um erro: Professor não foi Deletado'}, 404)
+        return jsonify({'erro': 'Professor não encontrado'}), 404
